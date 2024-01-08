26928079afbde330815e21fb636d8fa7bc525a79
Boka <Maxutca156@yandex.ru>
2024-01-08 18:24:36 +0300

    фц

--- lesson.py
+++ lesson.py
@@ -14,7 +14,7 @@ def money_changing():
     print('1. Доллары\n2. Евро\n3. Фунты стерлингов')
     while True:
         choice = input('Выберите валюту(1/2/3): ').lower()
-        if choice ==  '1':
+        if choice == '1':
             money = int(input('Введите вашу сумму в рублях: '))
             print('Вы получите долларов:' ,dollars(money))
         elif choice == '2':
